3be6e856399d1d8f262c628dd6846ad8897d492b
Phool Dx <59568224+Phool-Frontend@users.noreply.github.com>
2021-02-06 12:32:32 -0500

    Update conexion.php

--- php/conexion.php
+++ php/conexion.php
@@ -2,7 +2,7 @@
 
     function conexion(){
         $conexion = mysqli_connect(
-            'localhost','root','','codigo_barra',
+            'bmjtucbebpjgwngr8iou-mysql.services.clever-cloud.com','ujaxijtkq9hzgap8','y7mUc4Rhw0TalOkBnvl5','bmjtucbebpjgwngr8iou',
 
         );
 
